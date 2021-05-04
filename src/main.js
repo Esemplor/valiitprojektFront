4d362b01ce95451de2aebc6fc1a271c2a92c4d18
Mart-Matteus Kampus <mkampus@gmail.com>
2021-05-04 20:21:28 +0300

    mart

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   router,
+//   render: function (h) {
+//     return h(App)
+//   }
+// }).$mount('#app')
