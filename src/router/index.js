85256b41ca75095bdadd288b0c3a863546054d30
Mart-Matteus Kampus <mkampus@gmail.com>
2021-05-07 10:11:12 +0300

    lahendatud ingredient id saatmine

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import OptionSelectTest from "../views/OptionSelectTest";
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/',
+    name: 'OptionSelectTest',
+    component: OptionSelectTest
   },
   {
     path: '/about',
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,101 +1,118 @@
 <template>
   <div class="about">
+
+
     <h1>Kylmik.ee</h1>
+
+    <label class="typo__label">Vali koostisosad</label>
+    <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Otsi koostisosa"
+                 label="ingredient_name"
+                 track-by="ingredient_id" :options="options" :multiple="true" ></multiselect>
+    <pre class="language-json"><code>{{ value }}</code></pre>
+
     <h2>Vali koostisosad</h2>
-    <input type="checkbox" id="kartul" value=3 v-model="checkedIngredients">
-    <label for="kartul">Kartul</label>
-    <input type="checkbox" id="sai" value=4 v-model="checkedIngredients">
-    <label for="sai">Sai</label>
-    <input type="checkbox" id="juust" value=2 v-model="checkedIngredients">
-    <label for="juust">Juust</label>
-    <input type="checkbox" id="porgand" value=1 v-model="checkedIngredients">
-    <label for="porgand">Porgand</label>
-    <input type="checkbox" id="leib" value=5 v-model="checkedIngredients">
-    <label for="leib">Leib</label>
-    <input type="checkbox" id="või" value=6 v-model="checkedIngredients">
-    <label for="või">Või</label>
-    <input type="checkbox" id="kapsas" value=7 v-model="checkedIngredients">
-    <label for="kapsas">Kapsas</label>
-    <input type="checkbox" id="jahu" value=8 v-model="checkedIngredients">
-    <label for="jahu">Jahu</label>
-    <input type="checkbox" id="piim" value=9 v-model="checkedIngredients">
-    <label for="piim">Piim</label>
-    <input type="checkbox" id="muna" value=10 v-model="checkedIngredients">
-    <label for="muna">Muna</label>
-    <input type="checkbox" id="kanaliha" value=11 v-model="checkedIngredients">
-    <label for="kanaliha">Kanaliha</label>
-    <input type="checkbox" id="sealiha" value=12 v-model="checkedIngredients">
-    <label for="sealiha">Sealiha</label>
-    <input type="checkbox" id="makaronid" value=13 v-model="checkedIngredients">
-    <label for="makaronid">Makaronid</label>
-    <input type="checkbox" id="sibul" value=14 v-model="checkedIngredients">
-    <label for="sibul">Sibul</label>
-    <input type="checkbox" id="tomat" value=15 v-model="checkedIngredients">
-    <label for="tomat">Tomat</label>
-    <input type="checkbox" id="kurk" value=16 v-model="checkedIngredients">
-    <label for="kurk">Kurk</label>
-    <input type="checkbox" id="viinerid" value=17 v-model="checkedIngredients">
-    <label for="viinerid">Viinerid</label>
-    <input type="checkbox" id="riis" value=18 v-model="checkedIngredients">
-    <label for="riis">Riis</label>
-    <input type="checkbox" id="kala" value=19 v-model="checkedIngredients">
-    <label for="kala">Kala</label>
-    <input type="checkbox" id="veiseliha" value=20 v-model="checkedIngredients">
-    <label for="veiseliha">Veiseliha</label>
-    <input type="checkbox" id="suhkur" value=21 v-model="checkedIngredients">
-    <label for="suhkur">Suhkur</label>
-    <input type="checkbox" id="hapukoor" value=22 v-model="checkedIngredients">
-    <label for="hapukoor">Hapukoor</label>
-    <input type="checkbox" id="paprika" value=23 v-model="checkedIngredients">
-    <label for="paprika">Paprika</label>
-    <input type="checkbox" id="oliiviõli" value=24 v-model="checkedIngredients">
-    <label for="oliiviõli">Oliiviõli</label>
-    <input type="checkbox" id="tomatipüree" value=25 v-model="checkedIngredients">
-    <label for="tomatipüree">Tomatipüree</label>
-    <input type="checkbox" id="sool" value=26 v-model="checkedIngredients">
-    <label for="sool">Sool</label>
-    <input type="checkbox" id="küüslauk" value=27 v-model="checkedIngredients">
-    <label for="küüslauk">Küüslauk</label>
-    <input type="checkbox" id="pipar" value=28 v-model="checkedIngredients">
-    <label for="pipar">Pipar</label>
-    <input type="checkbox" id="kaalikas" value=29 v-model="checkedIngredients">
-    <label for="kaalikas">Kaalikas</label>
+
     <br>
     <button v-on:click="sendIngredients">Saada</button>
     <span></span>
     <br>
     <table border="1" style="border:1px solid black;margin-left:auto;margin-right:auto;">
-      <tr v-for="row in createAccountResponse">
-        <td><a :href= row.output > Link</a></td>
+      <tr v-for="row in getRecipesResponse">
+        <td><a :href=row.output> Link</a></td>
         <td>{{ row.recipeName }}</td>
       </tr>
     </table>
   </div>
-
 </template>
 <script>
+import Multiselect from 'vue-multiselect'
+
 
 function sendIngredients() {
-  this.$http.get('http://localhost:8080/stuff/recipe?a=' + this.checkedIngredients)
+  let x = this.value.map(y => y.ingredient_id);
+  this.$http.get('http://localhost:8080/stuff/recipe?a='+x)
       .then(response => {
-        this.createAccountResponse = response.data
+        this.getRecipesResponse = response.data
       })
       .catch(response => {
         alert("Error")
       })
 }
-
-
+function getArrayFromObject(){
+  const entries = Object.entries(this.value);
+  return entries;
+}
 export default {
-  data: function () {
+  components: {
+    Multiselect
+  },
+  data() {
     return {
+      value : '',
+      options: [
+        {ingredient_name: 'Kartul', ingredient_id: 1},
+        {ingredient_name: 'Kaalikas', ingredient_id: 29},
+        {ingredient_name: 'Must Pipar', ingredient_id: 28},
+        {ingredient_name: "kartul", ingredient_id: 3},
+        {ingredient_name: "sai", ingredient_id: 4},
+        {ingredient_name: "juust", ingredient_id: 2},
+        {ingredient_name: "porgand", ingredient_id: 1},
+        {ingredient_name: "leib", ingredient_id: 5},
+        {ingredient_name: "või", ingredient_id: 6},
+        {ingredient_name: "kapsas", ingredient_id: 7},
+        {ingredient_name: "jahu", ingredient_id: 8},
+        {ingredient_name: "piim", ingredient_id: 9},
+        {ingredient_name: "muna", ingredient_id: 10},
+        {ingredient_name: "kanaliha", ingredient_id: 11},
+        {ingredient_name: "sealiha", ingredient_id: 12},
+        {ingredient_name: "makaronid", ingredient_id: 13},
+        {ingredient_name: "sibul", ingredient_id: 14},
+        {ingredient_name: "tomat", ingredient_id: 15},
+        {ingredient_name: "kurk", ingredient_id: 16},
+        {ingredient_name: "viinerid", ingredient_id: 17},
+        {ingredient_name: "riis", ingredient_id: 18},
+        {ingredient_name: "kala", ingredient_id: 19},
+        {ingredient_name: "veiseliha", ingredient_id: 20},
+        {ingredient_name: "suhkur", ingredient_id: 21},
+        {ingredient_name: "hapukoor", ingredient_id: 22},
+        {ingredient_name: "paprika", ingredient_id: 23},
+        {ingredient_name: "oliiviõli", ingredient_id: 24},
+        {ingredient_name: "tomatipüree", ingredient_id: 25},
+        {ingredient_name: "sool", ingredient_id: 26},
+        {ingredient_name: "küüslauk", ingredient_id: 27},
+      ],
       'checkedIngredients': [],
-      'createAccountResponse': []
+      'getRecipesResponse': [],
+      'result':[],
+
+
     }
   },
   methods: {
-    'sendIngredients': sendIngredients
-
+    'sendIngredients': sendIngredients,
+    'getArrayFromObject': getArrayFromObject
   }
 }
 </script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
